35f6a60e211b1528406b7ab71c7467bf49ec33d4
gov6184 <gm040381@gmail.com>
2022-12-14 19:46:36 +0530

    d

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ let exp=require("express")
 let cors=require("cors")
 let app=exp()
 app.use(cors())
+let path=require("path")
 app.get("/",(req,res)=>{
     res.send("hello")
 })
 app.get("/resume",(req,res)=>{
-    res.download("./resume.pdf")
+    res.download(path.resolve("./resume.pdf"))
 })
 app.listen(8080)
